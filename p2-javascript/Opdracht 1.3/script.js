718f3576ecd4b814e9f9b1a1cacf0dc3b6b21c26
EthanPlaisier <9018008@student.zadkine.nl>
2023-11-23 09:51:32 +0100

    1.3

---
+++ p2-javascript/Opdracht 1.3/script.js
@@ -0,0 +1,7 @@
+function logUserInput() {
+    // Get the input element by its ID
+    var userInput = document.getElementById("productName").value;
+
+    // Log the user input to the console
+    console.log("User input:", userInput);
+}
